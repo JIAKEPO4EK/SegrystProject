bdaf723070e7c94b37a1f254efcc5ecc66586a23
JIAKEPO4EK <39829211+JIAKEPO4EK@users.noreply.github.com>
2018-06-22 18:14:10 +0300

    Update and rename js.html to script.js

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+ <script name="script">
+           'use strict'
+             function Complete()
+             {
+                var Elem="\nПол: " + document.Sel1.Sex.value +
+                   "\nРост: " + document.Sel1.Height.value +
+                   "\nВес: " + document.Sel1.Weight.value +
+                   "\nВозраст: " + document.Sel1.Age.value;
+               alert(Elem);
+          }
+</script>
