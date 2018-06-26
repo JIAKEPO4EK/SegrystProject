8811700e1c8f62a173a5aebb35af717ec998e0a2
JIAKEPO4EK <39829211+JIAKEPO4EK@users.noreply.github.com>
2018-06-26 18:32:29 +0300

    Update script.js
    Добавил смену картинок по кнопке.

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,33 @@
  <script name="script">
-           'use strict'
-             function Complete()
-             {
-                var Elem="\nПол: " + document.Sel1.Sex.value +
-                   "\nРост: " + document.Sel1.Height.value +
-                   "\nВес: " + document.Sel1.Weight.value +
-                   "\nВозраст: " + document.Sel1.Age.value;
-               alert(Elem);
-          }
-</script>
+      'use strict'
+       function Complete()
+    {
+      var Elem="\nПол: " + document.Sel1.Sex.value +
+      "\nРост: " + document.Sel1.Height.value +
+      "\nВес: " + document.Sel1.Weight.value +
+      "\nВозраст: " + document.Sel1.Age.value;
+      alert(Elem);
+    }
+     var flag = 0;
+  function changeImage() {
+    if (flag == 0) {
+      document.img.src = 'GBNП.jpg';
+      flag = 1;
+    }
+    else {
+      document.img.src = 'GBNТ.jpg';
+      flag = 0;
+    }
+  }
+      var flag = 0;
+  function Imagechang() {
+    if (flag == 0) {
+      document.img.src = 'QWEП.jpg';
+      flag = 1;
+    }
+    else {
+      document.img.src = 'QWEТ.jpg';
+      flag = 0;
+    }
+  }
+    </script>
